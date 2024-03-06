09dec7079ffc2f16445af398b799f97a15a56582
hjrbill <1326112622@qq.com>
2024-03-06 22:24:58 +0800

    feat: Add sentinels that control distributed workers

---
+++ index_service/sentinel.go
@@ -0,0 +1,213 @@
+package index_service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/hjrbill/quicker/pb"
+	qlog "github.com/hjrbill/quicker/pkg/log"
+	"github.com/hjrbill/quicker/pkg/service_hub"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
+	"google.golang.org/grpc/credentials/insecure"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// Sentinel 分布式 Work 的控制
+type Sentinel struct {
+	hub      service_hub.IHub // 服务注册中心
+	connPool sync.Map         // 连接池（与各个 IndexServiceWorker 的连接的缓存）
+}
+
+// NewSentinel 默认使用 HubProxy，可使用 WithHub 设置自定义 Hub
+func NewSentinel(etcdServers []string, loadBalancer service_hub.LoadBalancer, qps int) *Sentinel {
+	return &Sentinel{
+		hub:      service_hub.GetServiceHubProxy(etcdServers, 10, loadBalancer, qps),
+		connPool: sync.Map{},
+	}
+}
+
+// WithHub 设置自定义的 Hub
+func (s *Sentinel) WithHub(hub service_hub.IHub) *Sentinel {
+	s.hub = hub
+	return s
+}
+
+func (s *Sentinel) Close() (err error) {
+	s.connPool.Range(func(key, value interface{}) bool {
+		conn := value.(*grpc.ClientConn)
+		closeErr := conn.Close()
+		if closeErr != nil {
+			// 不返回 false（中断）,继续处理剩下的连接，但报告错误提醒
+			qlog.Errorf("关闭连接 %s 失败: %s", key, closeErr.Error())
+			err = closeErr
+		}
+		s.connPool.Delete(key) // 从连接池中删除，便于用户的二次处理
+		return true
+	})
+	s.hub.Close()
+	return err
+}
+
+func (s *Sentinel) GetGrpcConn(endpoint string) *grpc.ClientConn {
+	if v, ok := s.connPool.Load(endpoint); ok { // 从连接池中获取连接
+		conn := v.(*grpc.ClientConn)
+		if conn.GetState() == connectivity.Shutdown || conn.GetState() == connectivity.TransientFailure { // 如果连接已关闭或出于故障，则从连接池中删除
+			s.connPool.Delete(endpoint)
+			return nil
+		} else {
+			return conn
+		}
+	}
+	// 连接池中没有连接，尝试建立连接
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+	conn, err := grpc.DialContext(
+		ctx,
+		endpoint,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(), // 更改为同步连接，使得阻塞和超时控制生效
+	)
+	if err != nil {
+		qlog.Warnf("连接 %s 失败: %s", endpoint, err.Error())
+		return nil
+	}
+	s.connPool.Store(endpoint, conn)
+	qlog.Infof("成功与 %s 建立连接", endpoint)
+	return conn
+}
+
+// AddDocument 通过负载均衡算法获取可用的 IndexServiceWorker 并向其添加文档
+func (s *Sentinel) AddDocument(doc pb.Document) (int32, error) {
+	endpoint := s.hub.GetEndpoint(INDEX_SERVICE)
+	if endpoint == "" {
+		return 0, errors.New("没有可用的 IndexServiceWorker")
+	}
+	conn := s.GetGrpcConn(endpoint)
+	if conn == nil {
+		return 0, fmt.Errorf("连接 IndexServiceWorker %s 失败", endpoint)
+	}
+	affected, err := pb.NewIndexServiceClient(conn).AddDocument(context.Background(), &doc)
+	if err != nil {
+		return 0, err
+	}
+	qlog.Infof("向 IndexServiceWorker %s 添加文档共 %d 条", endpoint, affected.Count)
+	return affected.Count, nil
+}
+
+// DeleteDocument 向所有 IndexServiceWorker 广播，要求删除文档
+func (s *Sentinel) DeleteDocument(id string) int32 {
+	var affectedCount int32
+
+	endpoints := s.hub.GetEndpoints(INDEX_SERVICE)
+	if len(endpoints) == 0 {
+		return 0
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(len(endpoints))
+	for _, endpoint := range endpoints {
+		go func(endpoint string) {
+			defer wg.Done()
+			conn := s.GetGrpcConn(endpoint)
+			if conn != nil {
+				affected, err := pb.NewIndexServiceClient(conn).DeleteDocument(context.Background(), &pb.ID{ID: id})
+				if err != nil {
+					qlog.Warnf("从 IndexServiceWorker %s 删除文档失败: %s", endpoint, err.Error())
+				} else {
+					if affected.Count > 0 {
+						atomic.AddInt32(&affectedCount, affected.Count)
+						qlog.Infof("从 IndexServiceWorker %s 删除文档共 %d 条", endpoint, affected.Count)
+					}
+				}
+			}
+		}(endpoint)
+	}
+	wg.Wait()
+	return atomic.LoadInt32(&affectedCount)
+}
+
+// Search 向所有 IndexServiceWorker 广播，进行检索，并合并结果
+func (s *Sentinel) Search(query *pb.TermQuery, onFlag, offFlag uint64, orFlags []uint64) []*pb.Document {
+	endpoints := s.hub.GetEndpoints(INDEX_SERVICE)
+	if len(endpoints) == 0 {
+		return nil
+	}
+
+	docs := make([]*pb.Document, 0, len(endpoints)*10)
+	result := make(chan *pb.Document, len(endpoints)*10)
+	wg := sync.WaitGroup{}
+	wg.Add(len(endpoints))
+	for _, endpoint := range endpoints {
+		go func(endpoint string) {
+			defer wg.Done()
+			conn := s.GetGrpcConn(endpoint)
+			if conn != nil {
+				docs, err := pb.NewIndexServiceClient(conn).Search(context.Background(), &pb.SearchRequest{
+					TermQuery: query,
+					OnFlag:    onFlag,
+					OffFlag:   offFlag,
+					OrFlag:    orFlags,
+				})
+				if err != nil {
+					qlog.Warnf("从 IndexServiceWorker %s 搜索文档失败: %s", endpoint, err.Error())
+				} else {
+					if len(docs.Documents) > 0 {
+						qlog.Infof("从 IndexServiceWorker %s 搜索得到文档共 %d 条", endpoint, len(docs.Documents))
+						for _, doc := range docs.Documents {
+							result <- doc
+						}
+					}
+				}
+			}
+		}(endpoint)
+	}
+
+	resultFinish := make(chan struct{}, len(endpoints)*10)
+	go func() {
+		for {
+			doc, ok := <-result
+			if !ok {
+				break
+			}
+			docs = append(docs, doc)
+		}
+		resultFinish <- struct{}{}
+	}()
+	wg.Wait()
+	close(result)
+	<-resultFinish
+	return docs
+}
+
+// Count 向所有 IndexServiceWorker 广播，要求获取文档总数
+func (s *Sentinel) Count() int32 {
+	var count int32
+
+	endpoints := s.hub.GetEndpoints(INDEX_SERVICE)
+	if len(endpoints) == 0 {
+		return count
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(len(endpoints))
+	for _, endpoint := range endpoints {
+		go func(endpoint string) {
+			defer wg.Done()
+			conn := s.GetGrpcConn(endpoint)
+			if conn != nil {
+				affected, err := pb.NewIndexServiceClient(conn).Count(context.Background(), &pb.CountRequest{})
+				if err != nil {
+					qlog.Warnf("从 IndexServiceWorker %s 获取 doc count 失败: %s", endpoint, err.Error())
+				} else {
+					if affected.Count > 0 {
+						atomic.AddInt32(&count, affected.Count)
+						qlog.Infof("从 IndexServiceWorker %s 获取 doc count共 %d 条", endpoint, affected.Count)
+					}
+				}
+			}
+		}(endpoint)
+	}
+	wg.Wait()
+	return atomic.LoadInt32(&count)
+}
---
+++ index_service/test/sentinel_test.go
@@ -0,0 +1,142 @@
+package test
+
+import (
+	"fmt"
+	"github.com/hjrbill/quicker/index_service"
+	"github.com/hjrbill/quicker/internal/kvdb"
+	"github.com/hjrbill/quicker/pb"
+	qlog "github.com/hjrbill/quicker/pkg/log"
+	"github.com/hjrbill/quicker/pkg/service_hub"
+	"github.com/hjrbill/quicker/pkg/util"
+	"google.golang.org/grpc"
+	"net"
+	"strconv"
+	"testing"
+	"time"
+)
+
+var (
+	//在一台机器上启多个 worker，模拟实际生产中多台机器上启动多个 worker
+	etcdServers = []string{"127.0.0.1:2379"}
+	workPorts   = []int{5678, 5679, 5660}
+
+	workers []*index_service.IndexServiceWorker
+)
+
+func StartWorkers() {
+	workers = make([]*index_service.IndexServiceWorker, 0, len(workPorts))
+	for i, port := range workPorts {
+		// 监听本地端口
+		lis, err := net.Listen("tcp", "127.0.0.1:"+strconv.Itoa(port))
+		if err != nil {
+			panic(err)
+		}
+
+		server := grpc.NewServer()
+		service := new(index_service.IndexServiceWorker)
+		err = service.Init(50000, kvdb.BADGER, util.RootPath+"temp/book_badger_"+strconv.Itoa(i))
+		if err != nil {
+			panic(err)
+		}
+		service.Indexer.LoadFromForwardIndexFile() //从文件中加载索引数据
+		// 注册服务的具体实现
+		pb.RegisterIndexServiceServer(server, service)
+		err = service.Register(port, etcdServers, &service_hub.RoundRobin{})
+		if err != nil {
+			panic(err)
+		}
+		go func(port int) {
+			// 启动服务
+			qlog.Debugf("start grpc server on port %d\n", port)
+			err = server.Serve(lis) //Serve 会一直阻塞，所以放到一个协程里异步执行
+			if err != nil {
+				err := service.Close()
+				if err != nil {
+					qlog.Debugf("关闭 etcd 客户端失败: %v", err)
+				}
+				qlog.Debugf("start grpc server on port %d failed: %s\n", port, err)
+			} else {
+				workers = append(workers, service)
+			}
+		}(port)
+	}
+}
+
+func StopWorkers() {
+	for _, worker := range workers {
+		err := worker.Close()
+		if err != nil {
+			qlog.Debugf("关闭 etcd 客户端失败: %v", err)
+		}
+	}
+}
+
+func TestIndexCluster(t *testing.T) {
+	StartWorkers()
+	time.Sleep(3 * time.Second) //等所有 worker 都启动完毕
+	defer StopWorkers()
+
+	sentinel := index_service.NewSentinel(etcdServers, &service_hub.RoundRobin{}, 100)
+	//测试 Add 接口
+	book := Book{
+		ISBN:    "436246383",
+		Title:   "上下五千年",
+		Author:  "李四",
+		Price:   39.0,
+		Content: "冰雪奇缘 2 中文版电影原声带 (Frozen 2 (Mandarin Original Motion Picture",
+	}
+	doc := pb.Document{
+		Id:          book.ISBN,
+		BitsFeature: 0b10011, //二进制
+		Keywords:    []*pb.Keyword{{Field: "content", Word: "唐朝"}, {Field: "content", Word: "文物"}, {Field: "title", Word: book.Title}},
+		Bytes:       book.Serialize(),
+	}
+
+	n, err := sentinel.AddDocument(doc)
+	if err != nil {
+		qlog.Debugf("添加文档失败: %v", err)
+		t.Fail()
+	} else {
+		qlog.Debugf("添加%d个 doc\n", n)
+	}
+	//测试 Search 接口
+	query := pb.NewTermQuery("content", "文物")
+	query = query.And(pb.NewTermQuery("content", "唐朝"))
+	docs := sentinel.Search(query, 0, 0, nil)
+
+	docId := ""
+	if len(docs) == 0 {
+		qlog.Debug("无搜索结果")
+	} else {
+		for _, doc := range docs {
+			book := DeserializeBook(doc.Bytes) //检索的结果是二进流，需要自反序列化
+			if book != nil {
+				qlog.Debugf("%s %s %s %s %.1f\n", doc.Id, book.ISBN, book.Title, book.Author, book.Price)
+				docId = doc.Id
+			}
+		}
+	}
+
+	//测试 Delete 接口
+	if len(docId) > 0 {
+		n := sentinel.DeleteDocument(docId)
+		qlog.Debugf("删除%d个 doc\n", n)
+	}
+
+	//测试 Search 接口
+	docs = sentinel.Search(query, 0, 0, nil)
+	if len(docs) == 0 {
+		qlog.Debug("无搜索结果")
+	} else {
+		for _, doc := range docs {
+			book := DeserializeBook(doc.Bytes) //检索的结果是二进流，需要自反序列化
+			if book != nil {
+				qlog.Debugf("%s %s %s %s %.1f\n", doc.Id, book.ISBN, book.Title, book.Author, book.Price)
+			}
+			fmt.Println("book:", book)
+		}
+	}
+
+	println("end ................")
+	time.Sleep(3 * time.Second)
+}
